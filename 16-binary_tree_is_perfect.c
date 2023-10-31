b365e0b54555f1424931adc5d799331b79fd2165
khaoulaad <ouajaa.khaoula@gmail.com>
2023-10-31 14:56:14 -0400

    0

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,40 +1,40 @@
 #include "binary_trees.h"
 
 /**
- * compare_depth - compare size
+ * compare_depth - compares the size
  *
- * @tree: pointer to the root
+ * @tree: ptr to the root
  * Return: size
  */
-int compare_depth(const binary_tree_t *tree)
+int comp_depths(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
 	else
-		return (compare_depth(tree->left) + 1 + compare_depth(tree->right));
+		return (comp_depths(tree->left) + 1 + comp_depths(tree->right));
 }
 
 /**
- * binary_tree_is_perfect - function that checks if a binary tree is perfect
+ * binary_tree_is_perfect -  checks if a binary tree is perfect
  *
- * @tree: pointer to the root node of the tree to check
+ * @tree: ptr to the root node of the tree to check
  * Return: 0
  */
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int h_left = 0, h_right = 0;
+	int h_l = 0, h_r = 0;
 
-	if (!tree)
+	if (tree == NULL)
 		return (0);
 
 	if (tree && (!tree->left && !tree->right))
 		return (1);
 
-	h_left = compare_depth(tree->left);
-	h_right = compare_depth(tree->right);
+	h_l = comp_depths(tree->left);
+	h_r = comp_depths(tree->right);
 
-	if ((h_left - h_right) == 0)
+	if ((h_l - h_r) == 0)
 		return (1);
 
 	return (0);
